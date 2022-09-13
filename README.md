e9a37f1377f1416742e80fb76a581388e4756cb8
irinaa7 <113440880+irinaa7@users.noreply.github.com>
2022-09-13 15:28:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-zeu3j7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zeu3j7)
---
+++ index.js
@@ -0,0 +1,17 @@
+//let a = 5;
+//a=a+1;
+//let b=17;
+//b=a-1;
+//console.log(a);
+//console.log(b);
+// let a=!!'abc73f';
+// console.log(a);
+let a= Number(prompt('Vase godine?'));
+
+if (a>=18) {
+  console.log("Punoletni ste");
+} else {
+  console.log("Niste punoletni");
+}
+//   console.log(a*a);
+// if(typeof a =="number"){console.log(a*a)}
